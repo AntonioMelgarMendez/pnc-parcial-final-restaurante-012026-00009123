69cd61dbef5cd61c1aae0464b6593a18416d858e
AntonioMelgarMendez <00009123@uca.edu.sv>
2026-07-08 19:13:33 -0600

    test(ci/cd): testing github actions workflow

--- src/main/java/com/uca/repository/TableInfoRepository.java
+++ src/main/java/com/uca/repository/TableInfoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+//Prueba
 public interface TableInfoRepository extends JpaRepository<TableInfo, Long> {
     List<TableInfo> findByBranchId(Long branchId);
 }
